871b675fa56675ba37a938f26da2d5fd72dcbb22
alvintfl <alvintanfulong@gmail.com>
2022-05-15 01:45:46 +0800

    Added LevelUpMenu and a skill to modify player stats.

--- Game Spellslingers/Assets/Scripts/Player.cs
+++ Game Spellslingers/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : Character
 {
     [SerializeField] private HealthBar healthBar;
-    private static int maxHealth = 100;
+    public static int maxHealth = 100;
 
     Player() : base(10f, Player.maxHealth) { }
 
@@ -29,15 +29,22 @@ public class Player : Character
     }
 
     private void OnCollisonEnter2D(Collision2D collision) {
+        /*
         if (collision.gameObject.CompareTag(ENEMY_TAG)) {
             Debug.Log("hi");
             TakeDamage(10);
             Destroy(gameObject);
         }
+        */
     }
 
     public override void TakeDamage(int damage) {
         base.TakeDamage(damage);
         healthBar.SetHealth(CurrentHealth);
+    }
+
+    public void SetHealth(int health) {
+        CurrentHealth = health;
+        this.healthBar.SetHealth(health);
     }
 }
