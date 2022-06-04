36372e24d62a1dc1644198488d7a64ecbf0af10f
githubuser7 <yizhet73@gmail.com>
2022-06-04 23:11:58 +0800

    Fixed bug after merge 2

--- Game Spellslingers/Assets/Scripts/Skills/TheWeightoftheDraw.cs
+++ Game Spellslingers/Assets/Scripts/Skills/TheWeightoftheDraw.cs
@@ -3,31 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/** 
+* <summary>
+* A signature skill that massively increases damage in exchange for speed.
+* </summary>
+*/
+
 public class TheWeightoftheDraw : Skill
 {
-    /** 
-    * <summary>
-    * A signature skill that massively increases damage in exchange for speed.
-    * </summary>
-    */
-    private Shoot shoot;
+
+    private PlayerShoot shoot;
 
     public TheWeightoftheDraw() : base(1) { }
     public override void Start()
     {
         base.Start();
-        this.shoot = Player.instance.gameObject.GetComponent<Shoot>();
+        this.shoot = Player.instance.gameObject.GetComponent<PlayerShoot>();
         Button.onClick.AddListener(() =>
         {
             OnSelected(EventArgs.Empty);
-            this.shoot.IncreaseRate(-1f);
-            this.shoot.SetDamageMulti(3);
+            this.shoot.DecreaseRate(1f);
+            Arrow.SetDamageMulti(3);
             Arrow.ActivateStun();
         });
     }
     public override void Reset()
     {
-        this.shoot.SetDamageMulti(1);
+        Arrow.SetDamageMulti(1);
         Arrow.DeactivateStun();
     }
 }
